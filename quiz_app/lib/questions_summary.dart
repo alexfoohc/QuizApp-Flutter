e1f90b333388eb927d13a844e7c7ce76546990f8
Alejandro Hernandez <alex.hernandezc.21@gmail.com>
2023-05-30 23:23:03 -0600

    Style Questions Summary Screen

--- quiz_app/lib/questions_summary.dart
+++ quiz_app/lib/questions_summary.dart
@@ -14,16 +14,28 @@ class QuestionsSummary extends StatelessWidget {
           children: summaryData.map(
             (data) {
               return Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(((data['question_index'] as int) + 1).toString()),
                   Expanded(
                     child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: [
-                        Text(data['question'] as String),
+                        Text(
+                          data['question'] as String,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            // fontSize: 16,
+                            // fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
                         const SizedBox(
                           height: 5,
                         ),
-                        Text(data['chosen_answer'] as String),
+                        Text(
+                          data['chosen_answer'] as String,
+                        ),
                         Text(data['correct_answer'] as String),
                       ],
                     ),
